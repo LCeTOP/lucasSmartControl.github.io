03694b59933bffcba3bdf87110b12cd70a981f12
EJK6NL118 <ejk6nl118@edusesimg.com.br>
2026-08-26 15:45:54 -0300

    incompleto

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+let usuario = prompt ("Digite seu nome e sobrenome:");
+const agora = new Date();
+const dias = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+];
+let ano = agora.getFullYear();
+let mes = String(agora.getMonth() + 1).padStart(2, "0");
+let diaSemana = dias[agora.getDay()];
+let dia = String(agora.getDate()).padStart(2, "0");
+let hora = String(agora.getHours()).padStart(2, "0");
+let minuto = String(agora.getMinutes()).padStart(2, "0");
+let fuso = -agora.getTimezoneOffset();
+let sinal = fuso >= 0 ? "+" : "-";
+let horasFuso = String(Math.floor(Math.abs(fuso) / 60)).padStart(2, "0");
+let minutosFuso = String(Math.abs(fuso) % 60).padStart(2, "0");
+let dataAtual  = `${diaSemana}, ${dia}/${mes}/${ano} = ${hora}:${minuto} (${sinal}${horasFuso}:${minutosFuso})`;
